a8344eac9e932e7983a760a56fa366378cde5693
unknown <kevin.paredescaman20@gmail.com>
2022-02-21 18:40:45 -0500

    feat: test files added

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -4,13 +4,12 @@ import bcrypt from 'bcryptjs'
 import { CreateUserDto } from '../dtos/auths/request/signup.dto'
 import { UsersService } from '../services/user.service'
 
-
 export async function login(req: Request, res: Response): Promise<void> {
-  
-
-  res.status(200).json({message: "hola"})
+  res.status(200).json({ message: 'hola' })
 }
 
 export async function signup(req: Request, res: Response): Promise<void> {
-
+  if (req.body.password !== req.body.passwordConfirmation) {
+    res.send(400)
+  }
 }
