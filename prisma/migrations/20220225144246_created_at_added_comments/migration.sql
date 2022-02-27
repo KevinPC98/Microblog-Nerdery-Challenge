703da32e32cf2c626234fde6ba8d14f3d6492888
cheshire <coren114@gmail.com>
2022-02-26 23:04:19 -0500

    feat: added user to database

--- prisma/migrations/20220225144246_created_at_added_comments/migration.sql
+++ prisma/migrations/20220225144246_created_at_added_comments/migration.sql
@@ -9,3 +9,8 @@
 -- AlterTable
 ALTER TABLE "comments" ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+
+-- InsertUser
+INSERT INTO "users" ("id", "name", "user_name","email", "password", "is_name_public", "is_email_public","is_active",  "verified_at", "updated_at", "created_at", "role") VALUES
+('57be8001-27a5-409d-bc4f-ff5630ee88bb', 'Felipe', 'user1', 'kevinparedes@ravn.co', '$2a$10$ziRoWPWeqYPZZz5tP0hd4eIPFlYZAtQ2wTe1YHIbdp4cwhF.4Kz4e', false,false,false, NOW(),NOW(),NOW(),'U')
